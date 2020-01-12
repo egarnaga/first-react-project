56bc853a269a71f31e44c1da42648b10e513e384
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-01-12 15:35:33 +0600

    add header, navbar, profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg'/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
