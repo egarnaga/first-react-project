b32819bf38b91ab18da9e7f871c5269b20270137
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-01-25 06:03:26 +0600

    add redux

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {Route} from "react-router-dom";
-import {updateNewPostText} from "./redux/state";
+import {updateNewPostText} from "./redux/store";
 
 
 const App = (props) => {
-
   return (
       <div className='app-wrapper'>
         <Header />
