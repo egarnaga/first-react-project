f0eb0d8185e977606ba4f87ae8882add2c5c0406
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-02-06 21:37:31 +0600

    add auth

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from "./users.module.css";
 import userPhoto from '../../assets/images/user.png';
+import {NavLink} from "react-router-dom";
 
 
 let Users = (props) => {
@@ -23,7 +24,9 @@ let Users = (props) => {
       props.users.map( u => <div key={u.id}>
         <span>
           <div>
-            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={classes.userPhoto} />
+            <NavLink to={'/profile/' + u.id}>
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={classes.userPhoto} />
+            </NavLink>
           </div>
           <div>
             {u.followed
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {
-  followAC,
-  setCurrentPageAC,
-  setUsersAC,
-  setUsersTotalCountAC,
-  toggleIsFetchingAC,
-  unfollowAC
+  follow,
+  setCurrentPage,
+  setUsers,
+  setTotalUsersCount,
+  toggleIsFetching,
+  unfollow
 } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
-import preloader from '../../assets/images/preloader.svg';
 import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
@@ -57,7 +56,7 @@ let mapStateToProps = (state) => {
   }
 }
 
-let mapDispatchToProps = (dispatch) => {
+/*let mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {
       dispatch(followAC(userId));
@@ -78,6 +77,7 @@ let mapDispatchToProps = (dispatch) => {
       dispatch(toggleIsFetchingAC(isFetching));
     }
   }
-}
+}*/
 
-export default connect(mapStateToProps, mapDispatchToProps) (UsersContainer);
+export default connect(mapStateToProps,
+  {follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching}) (UsersContainer);
