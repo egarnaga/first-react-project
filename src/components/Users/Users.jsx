c575a3f7bdb49efe2341b6c814574df9d96701bc
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-02-10 20:20:29 +0600

    add api.js

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from "./users.module.css";
 import userPhoto from '../../assets/images/user.png';
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 
 let Users = (props) => {
@@ -30,9 +31,33 @@ let Users = (props) => {
           </div>
           <div>
             {u.followed
-              ? <button onClick={() => {props.unfollow(u.id)} }>Unfollow</button>
-              : <button onClick={() => {props.follow(u.id)} }>Follow</button>}
+              ? <button onClick={() => {
+                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                  withCredentials: true,
+                  headers: {
+                    'API-KEY': '2e7ea4e0-dcd7-428c-b177-79ec3d52c809'
+                  }
+                })
+                  .then(response => {
+                    if (response.data.resultCode == 0) {
+                      props.unfollow(u.id);
+                    }
+                  });
 
+              }}>Unfollow</button>
+              : <button onClick={() => {
+                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                  withCredentials: true,
+                  headers: {
+                    'API-KEY': '2e7ea4e0-dcd7-428c-b177-79ec3d52c809'
+                  }
+                })
+                  .then(response => {
+                    if (response.data.resultCode == 0) {
+                      props.follow(u.id);
+                    }
+                  });
+              }}>Follow</button>}
           </div>
         </span>
         <span>
